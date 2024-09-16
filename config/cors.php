bfa3f2dc0e08974368a0b5696482bb2bd48c0897
miguel000127@gmail.com <miguel000127@gmail.com>
2024-09-16 01:40:31 -0500

    [config] cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://devil-bit123.github.io'],
 
     'allowed_origins_patterns' => [],
 
